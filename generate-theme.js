d9211765269bb8b377a69494a09bb139524e9235
Kenneth Alvares <kennethalvares30399@gmail.com>
2024-02-12 21:53:12 +0530

    create-brix-theme now makes style.css in theme folder

--- generate-theme.js
+++ generate-theme.js
@@ -1,40 +1,51 @@
 #!/usr/bin/env node
 
-const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 
-if (process.argv.length < 3) {
-  console.log("Please only enter a single theme name like:\n\t\tnpx create-brix-theme myThemeName");
-  process.exit(1);
-}
+
+if (process.argv.length < 3 || process.argv.length > 3) {
+    console.log(
+      "Please only enter a single theme name like:\n\t\tnpx create-brix-theme myThemeName\n\t\tnpx create-brix-theme my-theme-name"
+    );
+    process.exit(1);
+  }
 
 const projectName = process.argv[2];
 process.chdir("..");
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
 
-try {
-    console.log("Creating theme folder...")
+async function main() {
+
+  try {
+    console.log("Creating theme folder...");
     fs.mkdirSync(projectPath);
     console.log("Created /" + projectPath);
-} catch (err) {
-  if (err.code === "EEXIST") {
-    console.log(
-      `/${projectName} already exists in the current directory, please give your theme another name.`
-    );
-  } else {
+    process.chdir(projectPath);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.log(
+        `/${projectName} already exists in the current directory, please give your theme another name.`
+      );
+    } else {
+      console.log(error);
+    }
+    process.exit(1);
+  }
+
+  try {
+    console.log("Creating style.css ...");
+    // console.log(projectPath+'./style.css');
+    fs.writeFileSync("./style.css", 'Hey there!');
+  } catch (err) {
     console.log(error);
   }
-  process.exit(1);
-}
 
-async function main() {
   try {
-    console.log("Creating theme files...");
-    
+    console.log("Moving to theme...");
+
     process.chdir(projectPath);
-    process
 
     console.log("Your Brix Theme is ready for use!");
   } catch (error) {
