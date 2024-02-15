974032728d617d8fd5c0adffc7f8e74333b56c84
Kenneth Alvares <kennethalvares30399@gmail.com>
2024-02-15 10:18:20 +0530

    Improved Stylesheet header

--- create-brix-theme.js
+++ create-brix-theme.js
@@ -160,16 +160,7 @@ function getStylesheetHeader(obj) {
   License: ${obj.license}
   License URI: ${obj.license_uri}
   Text Domain: ${obj.slug}
-  This theme is powered by the Brix Theme Development Kit (TDK)
-  
-  This theme, like WordPress, is licensed under the GPL.
-  Use it to make something cool, have fun, and share what you've learned.
-
-  AlphaTest is based on Underscores https://underscores.me/, (C) 2012-2020 Automattic, Inc.
-  Underscores is distributed under the terms of the GNU GPL v2 or later.
-
-  Normalizing styles have been helped along thanks to the fine work of
-  Nicolas Gallagher and Jonathan Neal https://necolas.github.io/normalize.css/
+  This theme is powered by the Brix Theme Development Kit (TDK), based on Underscores https://underscores.me/, (C) 2012-2020 Automattic, Inc. Underscores is distributed under the terms of the GNU GPL v2 or later. Normalizing styles have been helped along thanks to the fine work of Nicolas Gallagher and Jonathan Neal https://necolas.github.io/normalize.css/
 */\n\n`;
 }
 
